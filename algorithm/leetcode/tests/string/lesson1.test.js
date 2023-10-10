1ff7b712dce45a6bc13f540802b3b76b0ac31dfa
Haha Guan <hahaguan@gmail.com>
2023-10-10 16:10:01 +0800

    del: remove

--- algorithm/leetcode/tests/string/lesson1.test.js
+++
@@ -1,5 +0,0 @@
-import reseverWords from '../../code/string/lesson1.js';
-
-test("reseverWords: Let's take LeetCode contest",()=>{
-    expect(reseverWords("Let's take LeetCode contest")).toBe("s'teL ekat edoCteeL tsetnoc")
-})
